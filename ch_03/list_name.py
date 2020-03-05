e245a83c0232af6b3d0641840561ce5a764a1a67
Luk Web <lukwebdev@gmail.com>
2020-03-05 05:05:56 +0000

    add ch_03 file list_name.py

---
+++ ch_03/list_name.py
@@ -0,0 +1,18 @@
+names = ["Ewa", "Mark", "John", "Ola"]
+
+msg = f"{names[0]} is the best!"
+print(msg)
+
+msg = f"{names[1]} is the best!"
+print(msg)
+
+msg = f"{names[2]} is the best!"
+print(msg)
+
+msg = f"{names[3]} is the best!"
+print(msg)
+
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])
