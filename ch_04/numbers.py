80c1fa554bea4153b1fb48acba344436c4a1b859
Luk Web <lukwebdev@gmail.com>
2020-03-06 00:52:06 +0000

    add ch_04 files pizza.py and numbers.py

---
+++ ch_04/numbers.py
@@ -0,0 +1,32 @@
+# for x in range(1, 21):
+#     print(x)
+
+
+# milion = []
+# for x in range(1, 1000000):
+#     milion.append(x)
+# # print(milion)
+
+# numbers = list(range(1, 1000001))
+# print(min(numbers))
+# print(max(numbers))
+# print(sum(numbers))
+
+# odd_nr = list(range(1, 20, 2))
+# for x in odd_nr:
+#     print(x)
+
+# multiples = list(range(1, 31))
+# for x in multiples:
+#     print(x)
+
+# cubes = []
+# for x in range(1, 11):
+#     cube = x**3
+#     cubes.append(cube)
+# for cube in cubes:
+#     print(cube)
+
+# cubes = [value**3 for value in range(1, 11)]
+# for value in cubes:
+#     print(value)
---
+++ ch_04/pizza.py
@@ -0,0 +1,4 @@
+pizzas = ["peperoni", "ham", "veg"]
+for pizza in pizzas:
+    print(f"I like {pizza.title()} pizza!")
+print("Pizzaaaa!\n")
