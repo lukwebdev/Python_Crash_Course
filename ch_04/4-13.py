a8506e3832c211e97740995f4123003b2c77882e
Luk Web <lukwebdev@gmail.com>
2020-03-06 04:30:59 +0000

    add file 4-13

---
+++ ch_04/4-13.py
@@ -0,0 +1,9 @@
+menu_items = ("burger", "chowder", "filet steak", "cod", "salad")
+for item in menu_items:
+    print(item)
+
+# menu_items[0] = "hamburger"
+
+menu_items = ("burger", "chowder", "sirlion", "salmon", "salad")
+for item in menu_items:
+    print(item)
