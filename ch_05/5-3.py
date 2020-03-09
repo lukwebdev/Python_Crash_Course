5c5694bed7675d0cb368e50ec1a65580e29feabb
Luk Web <lukwebdev@gmail.com>
2020-03-09 19:50:06 +0000

    fix file 5-3.py

--- ch_05/5-3.py
+++ ch_05/5-3.py
@@ -1,10 +1,14 @@
-age = 66
-if age < 4:
-  price = 0
-elif age < 18:
-  price = 25
-elif age < 65:
-  price = 40
+# alien_color = ["green", "yellow", "red"]
+
+# if "green" in alien_color:
+#   print(Just earned 5 points!)
+
+alien_color = "green"
+if alien_color == "green":
+  print("Earned 5 points!")
+
+alien_color = "grey"
+if alien_color == "green":
+  print("Earned 5 points!")
 else:
-  price = 20
-print(f"Your admission cost is ${price}.")
+  print("False")
